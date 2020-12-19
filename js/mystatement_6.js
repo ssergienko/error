3ff1253d2a61939786286ff213c33588a9f77ae6
Yuri Pavlov <yuri@advascale.com>
2020-12-19 17:29:58 +0300

    Add Statement 6 which actually 3

---
+++ js/mystatement_6.js
@@ -0,0 +1,18 @@
+// On all counts I was to feel worse today. But I'm well.
+// It's illogic.
+
+let health = '';
+
+function sum(num1,num2) {
+  return num1 + num2;
+}
+
+health = sum(999, "Normal");
+
+
+if (parseInt(health)) {
+  document.write("<p>" + health + " is an Integer, therefore I am Normal</p>");
+}
+else {
+  document.write("I'm not Normal");
+}
