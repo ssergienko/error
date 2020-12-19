4ac5dc591b7d8fe7dbbf98d9d11ce00924a37995
Yuri Pavlov <yuri@advascale.com>
2020-12-19 18:10:45 +0300

    Add statement 7

---
+++ js/mystatement_7.js
@@ -0,0 +1,11 @@
+// I'm not at all sure already that my home is safe. 
+// It seems the walls are not absolutely vertical.
+
+var firstCheck = false;
+var secondCheck = false;
+
+let isHomeSafe = (firstCheck) ? true : false;
+let isWallVertical = (secondCheck) ? true : false;
+
+document.write("<p> My home is safe: " + isHomeSafe + "</p>");
+document.write("<p> All my walls are vertical: " + isWallVertical + "</p>");
