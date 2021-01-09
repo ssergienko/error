0988b61bace4c741f48340e9e69c26c1a189e897
Sergei Sergienko <sergey.s.sergienko@gmail.com>
2021-01-09 17:07:43 +0300

    test fix

--- src/components/Recursion/Window/Header/Header.js
+++ src/components/Recursion/Window/Header/Header.js
@@ -7,7 +7,7 @@ function Header({text}) {
     <div className="header">
       <div className="circle circle1"></div>
       <div className="circle circle2"></div>
-      <div className="circle circle3"></div>
+      <div className="circle circle3 test"></div>
     </div>
   );
 
