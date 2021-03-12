4eef8a4ab40703d2b083720ddc2f7074a0c3b9fe
Sergei Sergienko <sergey.s.sergienko@gmail.com>
2021-03-12 14:40:32 +0300

    walls

---
+++ src/components/Allipsis/Allipsis.js
@@ -0,0 +1,14 @@
+import './Allipsis.scss';
+import React from "react";
+
+function Soap() {
+
+  return (
+    <div className="allipsis-and-towel-wrapper">
+      <div className="ellipsis-anim"><span>.</span><span>.</span><span>.</span></div>
+    </div>
+  );
+
+}
+
+export default Soap;
