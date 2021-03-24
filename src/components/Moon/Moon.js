37181dacb993d56795120b047f2cbe014ff6636b
Sergei Sergienko <sergey.s.sergienko@gmail.com>
2021-03-24 07:04:54 +0300

    moon

---
+++ src/components/Moon/Moon.js
@@ -0,0 +1,16 @@
+import './Moon.scss';
+import React from "react";
+
+function Moon() {
+
+  return (
+    <div className="moon-wrapper">
+      <div className="moon">
+        <div className="disc"></div>
+      </div>
+    </div>
+  );
+
+}
+
+export default Moon;
