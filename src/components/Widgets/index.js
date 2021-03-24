2e4d8cf79f8d5dcd62114715d33b4b9ae2dc9dab
Sergei Sergienko <sergey.s.sergienko@gmail.com>
2021-03-24 07:18:41 +0300

    restructuring

---
+++ src/components/Widgets/index.js
@@ -0,0 +1,9 @@
+export {default as Allipsis} from './Allipsis/Allipsis';
+export {default as Book} from './Book/Book';
+export {default as Console} from './Console/Console';
+export {default as Counter} from './Counter/Counter';
+export {default as Dice} from './Dice/Dice';
+export {default as Moon} from './Moon/Moon';
+export {default as PrimaryKey} from './PrimaryKey/PrimaryKey';
+export {default as Recursion} from './Recursion/Recursion';
+export {default as Soap} from './Soap/Soap';
