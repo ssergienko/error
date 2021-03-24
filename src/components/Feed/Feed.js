94ad8d66758c9ac7303e0234af520de2f37240c2
Sergei Sergienko <sergey.s.sergienko@gmail.com>
2021-03-24 09:16:07 +0300

    infinit scroll

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -5,19 +5,35 @@ import Post from './Post/Post';
 import posts from '../../data.json';
 import './Feed.scss';
 
+const loadNextPage = (currentItems) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(currentItems.concat(posts));
+    }, 100);
+  });
+}
+
 function Feed() {
 
   let { id } = useParams();
   const [postsInfinitList, setPostsInfinitList] = React.useState(posts);
+  const [loading, setLoading] = React.useState(false);
   const refs = useRef([...new Array(postsInfinitList.length)].map(() => React.createRef()));
 
   const handleLoadMore = () => {
-    setPostsInfinitList(postsInfinitList.concat(postsInfinitList));
+    setLoading(true);
+    loadNextPage(postsInfinitList).then((newItems) => {
+      setLoading(false);
+      setPostsInfinitList(newItems);
+    });
   }
 
   const infiniteRef = useInfiniteScroll({
+    loading,
     hasNextPage: true,
-    onLoadMore: handleLoadMore
+    onLoadMore: handleLoadMore,
+    scrollContainer: 'window',
+    // threshold: 500
   });
 
   useEffect(() => {
@@ -30,7 +46,9 @@ function Feed() {
     <div className="feed" ref={infiniteRef}>
       {postsInfinitList.map((post, i) => 
         <Post post={post} key={i} myref={refs.current[i]} />
+        
       )}
+      {loading && <div>loading</div>}
     </div>
   );
 }
